4b37232107d51248b066a47ec3992151471c1ba1
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-14 09:25:56 -0300

    Chapter 16: add content to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
-import { Metadata } from "next";
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Customers'
-}
+  title: 'Customers',
+};
 
-export default function Customers() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
